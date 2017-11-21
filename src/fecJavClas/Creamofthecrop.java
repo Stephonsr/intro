12034af388e8a0a683055265a1b644e39044d4cb
JavaClass <JavaClass@15000549cpu.fec.org>
2017-11-21 14:54:48 -0600

    myfirst java program

---
+++ src/fecJavClas/Creamofthecrop.java
@@ -0,0 +1,11 @@
+package fecJavClas;
+
+public class Creamofthecrop {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This Is Stephon's");
+		System.out.println("The All Mighty Great");
+	}
+
+}
